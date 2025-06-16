84df6b853841c946729ade06ef22ce4bb57718ef
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2025-06-16 13:26:44 -0700

    Switching animation direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,27 +3,34 @@ extends CharacterBody2D
 const SPEED = 50.0
 const JUMP_VELOCITY = -300.0
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
+@onready var animation = $AnimatedSprite2D
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
+func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionx := Input.get_axis("ui_left", "ui_right")
+	var directionx := Input.get_axis("move_left", "move_right")
 	if directionx:
 		velocity.x = directionx * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var directiony := Input.get_axis("ui_up", "ui_down")
+	
+	var directiony := Input.get_axis("move_up", "move_down")
 	if directiony:
 		velocity.y = directiony * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	
+	#Determine the animation
+	#Right
+	if directionx < 0:
+		animation.flip_h = false
+		animation.play("walking")
+	elif directionx > 0:
+		animation.flip_h = true
+		animation.play("walking")
+	elif directiony != 0:
+		animation.play("walking")
+	else:
+		animation.play("idle")
+		#Note, the animation direction does not change.
 	move_and_slide()
